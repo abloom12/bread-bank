ed49e038562f363677ddc015610d6acd861b26cc
Ash <15804805+abloom12@users.noreply.github.com>
2025-12-14 15:26:21 -0500

    Starting feature development
    Moved example code for client and server into a __archive folder that is   in .gitignore

--- client/src/routes/users.$userId.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { UserDetailPage } from '../features/users/UsersDetailPage';
-
-export const Route = createFileRoute('/users/$userId')({
-  component: UserDetailPage,
-});
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 
 import { healthRouter } from './health.routes';
-import { authRouter } from './auth.routes';
 
 export const apiRouter = Router();
 
 apiRouter.use('/health', healthRouter);
-apiRouter.use('/auth', authRouter);
